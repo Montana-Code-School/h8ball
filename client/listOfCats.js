a80f2733acab0b2cccef6db4220f1270f3f94597
d228m3286w <d228m3286w@gmail.com>
2015-12-17 13:06:48 -0700

    strong search

--- client/listOfCats.js
+++ client/listOfCats.js
@@ -219,7 +219,7 @@ var AllCategories = React.createClass({
         return (
             <div>
                 <input type="text" placeholder="Search Categories" id="searching" ref="search"/>
-                <button className="button" onClick={self.props.searchCats}>Search</button>
+                <button className="button" onClick={self.props.searchCats}><strong id="strongSearch">Search</strong></button>
                 {cat}
             </div>
         );
